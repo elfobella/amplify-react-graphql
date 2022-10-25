8d154637349e9c4de954a6605259ebe53b9384bd
elfobella <yunusemreucr11@gmail.com>
2022-10-25 20:48:06 +0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> 66d6e5b (Initialize project using Create React App)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 66d6e5b (Initialize project using Create React App)
