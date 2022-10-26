67aa2229980d9cc0b38dd9d61ee4de34444e2d49
elfobella <yunusemreucr11@gmail.com>
2022-10-26 13:48:25 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+<<<<<<< HEAD
 import "./App.css";
 
 function App() {
   return <div className="App">Hello</div>;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> 66d6e5b (Initialize project using Create React App)
 }
 
 export default App;
